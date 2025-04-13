98db74ce61ac53bcab0245fb2a2f43eda94a8c05
AceBurton <acehburton@gmail.com>
2025-04-13 14:35:17 -0700

    flame temp with plot

--- ME432 Project.py
+++ ME432 Project.py
@@ -1,25 +1,32 @@
 import cantera as ct
+import matplotlib.pyplot as plt
 
 #Ace Burton, burtona25@up.edu
 #Adin Sokol, sokol25@up.edu
 
-gas = ct.Solution('gri30.cti')
-#air = ct.Solution('gri30.cti')
+gas = ct.Solution('gri30.yaml')
 
 #we can play with these
-fuel_ratio = 0.5
+#fuel_ratio = 0.5  # 0 = 100% NH3, 1 = 100% CH4
 gas_temp = 300
 air_ratio = 1.0
+fig, ax = plt.subplots()
 
-ammonia_ratio = 1-fuel_ratio
-fuel = 'CH4:fuel_ratio, NH3:ammonia_ratio'
-air = 'O2:1, N2:3.76'
-gas.TP = gas_temp, ct.one_atm
 
-gas.set_equivalence_ratio(air_ratio, fuel, air)
+for fuel_ratio in range(0, 100, 1):
+    fuel_ratio = fuel_ratio / 100.0  # Convert to a fraction
+    fuel = f'CH4:{fuel_ratio}, NH3:{1 - fuel_ratio}'
+    air = 'O2:1, N2:3.76'
+    gas.TP = gas_temp, ct.one_atm
 
-gas.equilibrate('HP')
+    gas.set_equivalence_ratio(phi = air_ratio, fuel = fuel, oxidizer = air)
 
-print(f"Adiabatic flame temperature: {gas.T:.1f} K")
+    gas.equilibrate('HP')
 
-#current version 11:10am
+
+    #print(f"Fuel mixture: {fuel}")
+    #print(f"Adiabatic flame temperature: {gas.T:.2f} K")
+    ax.plot([fuel_ratio], [gas.T], 'ro')
+
+plt.show()
+
